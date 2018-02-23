51cdb5b567b52f6eca11d041623547c296a96768
Crystal Hughes <hugachia@me.com>
2018-02-23 14:28:58 -0600

    images

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -4,36 +4,49 @@
 //path for search    /v1/gifs/search
 // gifButtons = my button id
 
-var api = "http://api.giphy.com/v1/gifs/search";
+
+var api = "";
 var apiKey = "&api_key=A7e1Em3Njn4PO001pMeyKhd37wsIxOH8";
 var query = "&q=actor"
-var actors=[];
+var actors = [];
+// Adding click event listen listener to all buttons
+$("gifButtons").on("click", function () {
+    // Grabbing and storing the data-animal property value from the button
+    var actorNames = $(this).attr("data-name");
+    console.log("data-name");
+    // Constructing a queryURL using the actorNames
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + actorNames() +
+        "&api-key=A7e1Em3Njn4PO001pMeyKhd37wsIxOH8";
 
-function actorNames() {
-var actorNames=$(this).attr("data-name");
-};
+    //performing AJax request with queryURL
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+    //what to do w/data after the request
+    .then(function(response){
+        console.log(queryURL);
+        console.log(response);
+        //storing data in the ajax request via variable
+        var results = response.data;
+        //create a loop for each result item
+        for(var i = 0; i < results.length; i++) {
+            //create & store div tag
+            var actorDiv = $("<div>");
+            //create a paragraph tag with results rating
+            var p = $("<p>").text("Rating: " + results[i].rating);
+            //create & store img tag
+            var actorImage = $("<img>");
+            //set src attr of image to a property pulled from JSON data
+            actorImage.attr("src", results[i].images.original.url);
+        }
+    })
+})
+
+
 //buttons rendering after search
-function renderbuttons(){
-    $("#gifButtons").empty();
-    for (var i=0; i < actorNames.length; i++){
-        //creating a button in html here//
-        var a=$("<button>");
-        a.addClass("actorButton");
-        a.attr("data-name".actors[i]);
-        a.text(actors[i])
-        $("#gifButtons").append(a);
-    }
-};
-var queryURL="http://api.giphy.com/v1/gifs/search?q="+actorNames()+"&api_key=A7e1Em3Njn4PO001pMeyKhd37wsIxOH8";
 
 
-
-
-//this data function will get filled w/info from api
-function gotData(data) {
-    printLn(data.data[0].images.original.url);
-
-}
 
 
 
